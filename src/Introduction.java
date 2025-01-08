494814d9143a45341cab72d894e3a471852b7d01
jarinjakia-20 <jarinjakia5215@gmail.com>
2025-01-08 08:18:49 +0600

    First commit

---
+++ src/Introduction.java
@@ -0,0 +1,11 @@
+public class Introduction {
+
+    public static void main(String[] args){
+
+        System.out.print(" Hello Java !!!! \n");
+        System.out.print("let's get ready !!!!!!");
+
+    }
+
+
+}
